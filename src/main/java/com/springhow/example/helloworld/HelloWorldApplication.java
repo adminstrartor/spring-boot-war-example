fe03a2b8a5fffdef4ac4057c2dbe99b2b8ad39aa
adminstrartor <159612643+adminstrartor@users.noreply.github.com>
2024-02-20 12:15:51 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "This devops batch (vinod and vandana) is learning Jenkins deployment and learning good!! ";
+        return "This devops batch is learning Jenkins deployment and learning good!! wow! with webhook ";
     }
 
 }
